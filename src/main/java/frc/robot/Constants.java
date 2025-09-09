e65a12d319ac7c5fc764d8234f1d1f925390cf9e
nlaverdure <nate.laverdure@gmail.com>
2025-09-09 18:43:58 -0400

    update auto select switch port numbers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -60,10 +60,10 @@ public final class Constants {
   }
 
   public static final class AutoConstants {
-    public static final int kAllianceColorSelectorPort = 3;
+    public static final int kAllianceColorSelectorPort = 10;
 
     // max length is 8
-    public static final int[] kAutonomousModeSelectorPorts = {0, 1, 2};
+    public static final int[] kAutonomousModeSelectorPorts = {11, 12, 13, 18, 19};
   }
 
   public static final class OIConstants {
