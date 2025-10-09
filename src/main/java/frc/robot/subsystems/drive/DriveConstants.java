931a733d7d18a616c2fd6a815d3997a49bc1c41e
nlaverdure <nate.laverdure@gmail.com>
2025-10-09 18:09:15 -0400

    tune steering

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -144,7 +144,7 @@ public class DriveConstants {
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
   private static final Slot0Configs steerGains =
       new Slot0Configs()
-          .withKP(100)
+          .withKP(300)
           .withKI(0)
           .withKD(0.5)
           .withKS(0.1)
@@ -154,7 +154,7 @@ public class DriveConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(10.0).withKI(0).withKD(0).withKS(0).withKV(0.124);
+      new Slot0Configs().withKP(10).withKI(0).withKD(0).withKS(0).withKV(0.124);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
